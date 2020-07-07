42722cac3b0e721d6976506b30cbe90b1fb8f37b
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-07 11:05:59 +0700

    new

---
+++ creds.py
@@ -0,0 +1,21 @@
+class Creds():
+    # ENTER Your bot Token Here
+    TG_TOKEN = "1252994934:AAHwr3kR7mCjXsa8GEcusrpc8cDgiIjkEQ8"
+    
+    
+    
+    #  Make Sure You Are Providing both value if you need Teamdrive upload
+    # Because of pydrive And pydrive v2 Api
+    
+    #Folder Id Of Teamdrive
+    TEAMDRIVE_FOLDER_ID = "1Gu7uHazFIlgMLNeKx1OlWJVl_PoZHTM2"
+    
+    # Id of Team drive 
+    TEAMDRIVE_ID = "0ALRjUZXTCkFlUk9PVA"
+    
+    
+    
+    #Example 
+    #TG_TOKEN = "dkjfksdkffdkfdkfdj"
+    #TEAMDRIVE_FOLDER_ID = "13v4MaZnBz-iEHlZ0gFXk7rh"
+    #TEAMDRIVE_ID = "0APh6R4WVvguEUk9PV"
